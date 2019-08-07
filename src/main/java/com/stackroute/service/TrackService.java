b60e095efbdc7196f02580755c4d7ae122d4e286
Vishal Pattnaik <vishalpattnaik387@gmail.com>
2019-08-07 11:01:58 +0530

    chnaged delete() return type

--- src/main/java/com/stackroute/service/TrackService.java
+++ src/main/java/com/stackroute/service/TrackService.java
@@ -10,15 +10,12 @@ public interface TrackService{
 
     public Track saveTrack(Track track) throws TrackAlreadyExistsException;
 
-    public Track getTrackById(int id);
+    public Track getTrackById(int id) throws TrackNotFoundException ;
 
-    public void deleteTrack(int id) throws TrackNotFoundException;
+    public List<Track> deleteTrack(int id) throws TrackNotFoundException;
 
-    public List<Track> getAllTracks();
+    public List<Track> getAllTracks() throws TrackNotFoundException ;
 
-    public Track updateTrack(int id,String comment);
+    public Track updateTrack(int id,String comment) throws TrackNotFoundException ;
 
-    List<Track> getByTrackName(String name);
-
-    List<Track> getByTrackNameSortByName(String name);
 }
