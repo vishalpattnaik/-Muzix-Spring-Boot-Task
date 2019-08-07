460fde314c579a1dd57a06ada3ea5ac6ce880da5
Vishal Pattnaik <vishalpattnaik387@gmail.com>
2019-08-07 11:23:52 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -34,12 +34,20 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public List<Track> getAllTracks() {
+    public List<Track> getAllTracks() throws TrackNotFoundException {
+
+        if(!trackRepository.existsById(id)) {
+            throw new TrackNotFoundException("No track found with given ID");
+        }
         return trackRepository.findAll();
     }
 
     @Override
-    public Track getTrackById(int id) {
+    public Track getTrackById(int id) throws TrackNotFoundException {
+
+        if(!trackRepository.existsById(id)) {
+            throw new TrackNotFoundException("No track found with given ID");
+        }
 
         Track track = trackRepository.findById(id).get();
         return track;
@@ -66,16 +74,6 @@ public class TrackServiceImpl implements TrackService {
         Track savedTrack = trackRepository.save(track1);
         return savedTrack;
 
-    }
-
-    @Override
-    public List<Track> getByTrackName(String name) {
-        return trackRepository.findByName(name);
-    }
-
-    @Override
-    public List<Track> getByTrackNameSortByName(String name) {
-        return trackRepository.findByNameSortById(name);
     }
 
 }
