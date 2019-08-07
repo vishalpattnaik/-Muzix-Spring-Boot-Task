02789482a80e95c45bad29ed7f14ab45f75e5ff8
Vishal Pattnaik <vishalpattnaik387@gmail.com>
2019-08-07 12:20:32 +0530

    Update TrackController.java

--- src/main/java/com/stackroute/controller/TrackController.java
+++ src/main/java/com/stackroute/controller/TrackController.java
@@ -12,6 +12,7 @@ public class TrackController {
 
     TrackService trackService;
 
+    @Autowired
     public TrackController(TrackService trackService) {
         this.trackService = trackService;
     }
