18c5e27771ec0488e1efa164d1f906d0092e7c03
Vishal Pattnaik <vishalpattnaik387@gmail.com>
2019-08-07 11:08:02 +0530

    changed delete() return type

--- src/main/java/com/stackroute/service/TrackService.java
+++ src/main/java/com/stackroute/service/TrackService.java
@@ -10,16 +10,12 @@ public interface TrackService{
 
     public Track saveTrack(Track track) throws TrackAlreadyExistsException;
 
-    public Track getTrackById(int id);
+    public Track getTrackById(int id) throws TrackNotFoundException;
 
-    public boolean deleteTrack(int id) throws TrackNotFoundException;
+    public List<Track> deleteTrack(int id) throws TrackNotFoundException;
 
-    public List<Track> getAllTracks();
+    public List<Track> getAllTracks() throws TrackNotFoundException;
 
     public Track updateTrack(Track track) throws TrackNotFoundException;
-
-    List<Track> getByTrackName(String name);
-
-    List<Track> getByTrackNameSortByName(String name);
 
 }
